048a463e96e80122d439f61ab4ac186753e44220
jln8936 <jln8936@nyu.edu>
2026-09-08 21:28:14 -0500

    Add files via upload

---
+++ gettingStarted.py
@@ -0,0 +1,50 @@
+### welcome_assignment_answers
+### Input  - All nine questions given in the assignment.
+### Output - The right answer for the specific question.
+
+def welcome_assignment_answers(question):
+    # Students do not have to follow the skeleton for this assignment.
+    # Another way to implement is using "case" statements similar to C.
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "02aec86e56f175da1ad309f0dd5c20c4ae036e25d409e8f2e57ac0abd65d3a67"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = 5
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 3
+    else:
+        # Included so a typo in a question string is obvious instead of crashing
+        # or silently returning the wrong thing.
+        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+    return answer
+
+
+# Complete all the questions.
+if __name__ == "__main__":
+    # Use this space to debug and verify that the program works
+    questions = [
+        "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?",
+        "Are encoding and encryption the same? - Yes/No",
+        "Is it possible to decrypt a message without a key? - Yes/No",
+        "Is it possible to decode a message without a key? - Yes/No",
+        "Is a hashed message supposed to be un-hashed? - Yes/No",
+        "What is the SHA256 hashing value of your NYU email and use the answer in your code - ",
+        "Is MD5 a secured hashing algorithm? - Yes/No",
+        "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number",
+        "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number",
+    ]
+    for q in questions:
+        print(q)
+        print(" ->", welcome_assignment_answers(q))
+        print()
